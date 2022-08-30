4e738d7bbc935e335e2cd29fa653d5c5edcde53d
amcassano <54558244+amcassano@users.noreply.github.com>
2022-08-30 14:31:57 -0500

    edit results function

--- R/get_results.r
+++ R/get_results.r
@@ -1,23 +1,19 @@
 #' Get DE results
-#' 
+#'
 #' Get the results of a deseq object
 #'
 #' @param contrast_str string, defaults to "Condition", the comparison of interest for pairwise comparison
 #' @param numerator string, name of the conditon of interest for comparison
 #' @param denominator string, name of baseline condition for comparison
 #' @param tidy_result boolean, defaults to TRUE, dictates if the results are returned as a dataframe or obj
-#' @param deseq_obj data 
+#' @param deseq_obj data
 #'
 #' @return DESeq Results object, or a dataframe
 #' @export
 get_DEresults <- function(contrast_str = "Condition", numerator, denominator, tidy_result = TRUE, deseq_obj){
   con <- c(contrast_str, numerator, denominator)
   results <- DESeq2::results(deseq_obj, con, tidy = tidy_result)
-  
-  if (tidy_result) {
-    results <- dplyr::rename("GeneID" = "row")
-  }
-  
+
   return(results)
-  
+
 }
